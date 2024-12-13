07aa718957dc2c758737edb8e9d01d8dd3650e0e
saltukalakus <saltukalakus@gmail.com>
2024-12-13 22:18:20 +0000

    update visitor pattern code

--- src/patterns/behavioral/visitor/src/main.rs
+++ src/patterns/behavioral/visitor/src/main.rs
@@ -1,27 +1,34 @@
 // Define the Visitor trait
 trait Visitor {
-    fn visit_element_a(&mut self, element: &ElementA);
-    fn visit_element_b(&mut self, element: &ElementB);
+    fn multiply_by_two(&mut self, element: &mut ElementA);
+    fn add_ten(&mut self, element: &mut ElementB);
 }
 
 // Define the Element trait
 trait Element {
-    fn accept(&self, visitor: &mut dyn Visitor);
+    fn accept(&mut self, visitor: &mut dyn Visitor);
 }
 
 // Define concrete elements
-struct ElementA;
-struct ElementB;
+struct ElementA {
+    data: i32,
+}
+
+struct ElementB {
+    data: i32,
+}
 
 impl Element for ElementA {
-    fn accept(&self, visitor: &mut dyn Visitor) {
-        visitor.visit_element_a(self);
+    fn accept(&mut self, visitor: &mut dyn Visitor) {
+        visitor.multiply_by_two(self);
+        println!("After the visit: {:?}", self.data);
     }
 }
 
 impl Element for ElementB {
-    fn accept(&self, visitor: &mut dyn Visitor) {
-        visitor.visit_element_b(self);
+    fn accept(&mut self, visitor: &mut dyn Visitor) {
+        visitor.add_ten(self);
+        println!("After the visit: {:?}", self.data);
     }
 }
 
@@ -29,20 +36,22 @@ impl Element for ElementB {
 struct ConcreteVisitor;
 
 impl Visitor for ConcreteVisitor {
-    fn visit_element_a(&mut self, element: &ElementA) {
-        println!("Visiting ElementA");
+    fn multiply_by_two(&mut self, element: &mut ElementA) {
+        println!("Visiting ElementA: {:?}", element.data);
+        element.data *= 2;
     }
 
-    fn visit_element_b(&mut self, element: &ElementB) {
-        println!("Visiting ElementB");
+    fn add_ten(&mut self, element: &mut ElementB) {
+        println!("Visiting ElementB: {:?}", element.data);
+        element.data += 10;
     }
 }
 
 fn main() {
-    let elements: Vec<Box<dyn Element>> = vec![Box::new(ElementA), Box::new(ElementB)];
+    let elements: Vec<Box<dyn Element>> = vec![Box::new(ElementA { data: 10 }), Box::new(ElementB { data: 5 })];
     let mut visitor = ConcreteVisitor;
 
-    for element in elements {
+    for mut element in elements {
         element.accept(&mut visitor);
     }
 }
